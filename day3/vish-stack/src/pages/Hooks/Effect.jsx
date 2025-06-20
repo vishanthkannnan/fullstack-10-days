5e6695c73bf7595a4442d79750fdaf02bbb266fc
vishanthannan <vishanthkannan777@gmail.com>
2025-06-20 16:05:31 +0530

    day-4

--- day3/vish-stack/src/pages/Hooks/Effect.jsx
+++ day3/vish-stack/src/pages/Hooks/Effect.jsx
@@ -1,27 +1,43 @@
 import { useState, useEffect } from "react";
 
 const Effect = () => {
-  const [count, setCount] = useState(0);
-  const [posts, setPosts] = useState([]); 
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())     
-      .then((data) => setPosts(data)); 
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
   }, []);
 
   useEffect(() => {
-    console.log(posts);              
-  }, [posts]);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  // Logs whenever posts or users update
+  useEffect(() => {
+    console.log('Users:', users);
+    console.log('Posts:', posts);
+  }, [users, posts]);
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
 
+      <h2>Posts</h2>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
         ))}
       </ul>
     </div>
